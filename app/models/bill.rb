032c68341fc22762566b64024dc162076b434469
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-06-10 19:56:19 +0100

    Update bill.rb

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,7 @@
 class Bill < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :bills, dependent: :destroy
+
+  validates :name, presence: true
+  validates :amount, presence: true
 end
